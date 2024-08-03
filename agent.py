49fe52e243b13fb768bceb020665153c27a658d9
shny0711 <kai.shunya@lm.sys.i.kyoto-u.ac.jp>
2024-08-03 11:40:12 +0900

    from MORIMOTOken

--- agent.py
+++ agent.py
@@ -1,7 +1,8 @@
 # DQNエージェント
 import random
 from collections import deque
-from net import Qnetwork
+from net import QNetwork
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -16,6 +17,35 @@ class DQNAgent:
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
-        self.model = Qnetwork(state_size, action_size)
-        self.target_model = Qnetwork(state_size, action_size)
+        self.model = QNetwork(state_size, action_size)
+        self.target_model = QNetwork(state_size, action_size)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+    def update_target_network(self):
+        self.target_model.load_state_dict(self.model.state_dict())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        state = torch.FloatTensor(state).unsqueeze(0)
+        act_values = self.model(state)
+        return np.argmax(act_values.detach().numpy()[0])
+    
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                next_state = torch.FloatTensor(next_state).unsqueeze(0)
+                target += self.gamma * np.amax(self.target_model(next_state).detach().numpy()[0])
+                target_f = self.model(torch.FloatTensor(state).unsqueeze(0)).detach().numpy()
+                target_f[0][action] = target
+                self.optimizer.zero_grad()
+                loss = nn.MSELoss()(self.model(torch.FloatTensor(state).unsqueeze(0)), torch.FloatTensor(target_f))
+                loss.backward()
+                self.optimizer.step()
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
--- num_sort.py
+++ num_sort.py
@@ -3,65 +3,29 @@
 # s_t: N桁の数字 
 # a_t: x_i, x_(i+1)を入れ替える, 確率1でa_tによるs_(t+1)が生成される
 # r_t: 数列を昇順にソートできたら報酬1, それ以外の場合には -2/(N(N-1))
+from env import SortEnv
+from agent import DQNAgent
+from tqdm import tqdm
 
-# 暫定値
-N = 54321
-x_list = [int(x) for x in str(N)]
-A = 1000
-B = 100
-sorted_list = sorted(x_list)
+env = SortEnv([5,4,1,1])
+agent = DQNAgent(env.N, env.N-1)
+episodes = 1000
+bach_size = 32
 
-# action
-def swap(x_list, i):
-    x_list[i], x_list[i+1] = x_list[i+1], x_list[i]
-    return x_list
-
-def step(self, act):
-    change_index = act
-    state = self.state
-    state = swap(state, change_index)
-    if state == self.sorted_list:
-        self.reward = 1
-        self.state = state
-        return state, self.reward, True
-    else:
-        self.reward += -2/(N*(N-1))
-        self.state = state
-        return state, self.reward, False
-    
-
-def reward(x_list):
-    return
-
-def choose_action(state):
-    # ε-greedy法で行動を選択
-    return
-
-# Q関数をNNによる非線形関数近似手法を用いる。
-# Q(s,a)がパラメータθで表現、近似した関数をQ_θ(s,a)とする。
-# Q_θ(s,a)と同一のNNアーキテクチャを持つ関数Q^hat_θ(s,a)を用意する。
-
-def main():
-    # θ、キューPの初期化、theta_bar = theta, i=0
-    th = th_init
-    P = []
-    for i in range(10):
-        t = 0
-        s = [int(x) for x in str(N)]
-        for t in range(10):
-            act = choose_action(s)
-            n_s, r, done = env.step(act)
-            
-            P.append([s, act, r, n_s])
-            
-            if i > A:
-                # Q関数をθについて更新
-                return
-            if i % B == 0:
-                # θ_bar = θ
-                return
-            
+with tqdm(range(episodes)) as pbar:
+    for e in pbar:
+        state = env.reset()
+        for time in range(500):
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            print("time:",time)
+            print("state:",state)
+            print("action:",action)
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
             if done:
+                agent.update_target_network()
+                print(f"episode: {e}/{episodes}, score: {time}, e: {agent.epsilon:.2}, reward: {reward}")
                 break
-            
-            s = n_s
+            if len(agent.memory) > bach_size:
+                agent.replay(bach_size)
